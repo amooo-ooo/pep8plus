e93b40bc6f8ab1d31cfca90e1980b71df1976759
amoooooo <83256424+amooo-ooo@users.noreply.github.com>
2023-11-04 16:59:42 +1300

    Update url.py

--- src/url.py
+++ src/url.py
@@ -2,21 +2,20 @@ from pathlib import Path
 import json
 RULES_DIR = Path(Path(__file__).parent, "rules")
 
-def encode(disabled: list, chars: str, linter: str = 'ruff') -> str:
+def encode(disabled: list, chars: str, linter: str = 'flake8') -> str:
     linter = linter.lower()
 
     # set settings based on linter
-    with open(Path(RULES_DIR, linter+'.json '), 'r') as f:
+    with open(Path(RULES_DIR, linter+'.json'), 'r') as f:
         settings = json.loads(f.read())
 
     binary = ''
     for category in settings:
         for code in settings[category]:
              binary += str(int(code in disabled))
-             if code in disabled:
-                 print(code)
+
+    print(binary)
     n = int(binary, 2)
-        
     e = len(chars)
 
     if n <= e: return chars[n]
@@ -28,28 +27,28 @@ def encode(disabled: list, chars: str, linter: str = 'ruff') -> str:
     return ''.join(digits[::-1])
 
 
-def decode(code: str, chars: str, linter: str) -> dict:
+def decode(code: str, chars: str, linter: str = 'flake8') -> dict:
     linter = linter.lower()
 
     # set settings based on linter
-    with open(Path(RULES_DIR, linter+'.json '), 'r') as f:
+    with open(Path(RULES_DIR, linter+'.json'), 'r') as f:
         settings = json.loads(f.read())
 
     if code == 'all':
         return settings
-    
-    total_errors = len(settings.values())
+
+    total_errors = sum(len(i) for i in settings.values())
 
     n = 0
     for char in code:
         n = n * len(chars) + chars.index(char)
+
     n = str(bin(n)[2:])
-    n = '0'*(total_errors-len(n)) + n
+    n = ('0'*(total_errors-len(n))) + n
 
     index = 0
     for category in settings:
         for code in settings[category]:
-             settings[category][code]['value'] = not bool(int(n[index]))
-             index += 1
-
+            settings[category][code]['value'] = not bool(int(n[index]))
+            index += 1
     return settings
