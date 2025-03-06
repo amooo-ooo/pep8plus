f7f88a4194ca5fba04b9f74f6dd64c98e78de8d5
amoooooo <amor.budiyanto2006@gmail.com>
2025-03-07 00:21:58 +1300

    chore: api refactor & improvements, web bug fixes

--- api/server.py
+++ api/server.py
@@ -1,66 +1,51 @@
+__all__ = [
+    "Api",
+    "TOOLS"
+]
+
+from functools import partial
+from typing import List, Tuple, Callable, Optional
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from lib.url import encode, decode
-from lib.linter import lint, fix
+from lib.utils import encode, decode, lint, fix
 
-app = Flask(__name__, static_folder='client/dist', static_url_path="/")
-CORS(app)          
-                    
-# Global default values   
-CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-+!"
+TOOLS: List[Tuple[str, Callable]] = [
+    ("/check", lint),
+    ("/autofix", fix),
+    ("/encode", encode),
+    ("/decode", decode)
+]
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return app.send_static_file("index.html")
+class Api(Flask):
+    def __init__(self, tools: Optional[List[Tuple[str, Callable]]] = None):
+        super().__init__(__name__, static_folder='client/dist', static_url_path="/")
+        CORS(self)
+        self.setup_routes(tools or TOOLS)
 
-@app.route('/check', methods=['POST'])
-def process_data():
-    data = request.get_json(force=True)
-    if not data:
-        return jsonify({'error': 'no JSON data sent'}), 400
-    
-    
+    def setup_routes(self, tools: List[Tuple[str, Callable]]):
+        self.add_url_rule('/', defaults={'path': ''}, view_func=self.catch_all)
+        self.add_url_rule('/<path:path>', view_func=self.catch_all)
+        
+        # TODO: must be a cleaner way
+        for path, method in tools:
+            view_func = partial(self.handle, method)
+            view_func.__name__ = method.__name__ 
+            self.add_url_rule(path, methods=['POST'], view_func=view_func)
 
-    errors = lint(code=data.get('code', ''),
-                  linter=data.get('linter', 'flake8'),
-                  disable=data.get('disable', None))
+    def catch_all(self, path: str = "index.html"):
+        return self.send_static_file(path)
 
-    return jsonify(errors), 200
+    def handle(self, func: Callable):
+        data = request.get_json(force=True)
+        if not data:
+            return jsonify({'error': 'no JSON data sent'}), 400
+        return jsonify(func(**data)), 200
 
-@app.route('/autofix', methods=['POST'])
-def autofix():
-    data = request.get_json(force=True)
-    if not data:
-        return jsonify({'error': 'no JSON data sent'}), 400
-
-    fixed = fix(code=data.get('code', ''))
-
-    return jsonify(fixed), 200
-
-@app.route('/encode', methods=['POST'])
-def construct():
-    data = request.get_json(force=True)
-    if not data:
-        return jsonify({'error': 'no JSON data sent'}), 400
-
-    errors = {"link": encode(linter=data.get('linter', 'flake8'),
-                             disabled=data.get('disabled', None),
-                             chars=CHARS), }
-    return jsonify(errors), 200
-
-@app.route('/decode', methods=['POST'])
-def deconstruct():
-    data = request.get_json(force=True)
-    if not data:
-        return jsonify({'error': 'no JSON data sent'}), 400
-
-    errors = decode(code=data.get('settings', 'all'),
-                    chars=CHARS,
-                    linter=data.get('linter', 'flake8'))
-
-    return jsonify(errors), 200
+def main():    
+    app_instance = Api(TOOLS)
+    app_instance.run()
 
 if __name__ == '__main__':
-    app.run()
+    main()
