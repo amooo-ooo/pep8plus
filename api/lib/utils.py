d7fe88cff87c522b5486da1399c9f68664d20a07
Amor Budiyanto <amor.budiyanto@gmail.com>
2025-06-17 14:37:40 +1200

    feat(rules): automatically seed latest pylint rules, migration from json to sqlite

--- api/lib/utils.py
+++ api/lib/utils.py
@@ -1,33 +1,29 @@
 __all__ = [
-    "RULES_DIR",
     "DISABLE_KEYS",
     "LINTER_REGEX",
-    "load_settings",
+    "RULES_DIR",
     "SETTINGS",
-    "encode",
     "decode",
-    "wrap",
+    "encode",
+    "fix",
     "lint",
-    "fix"
+    "load_settings",
+    "wrap",
 ]
 
-from pathlib import Path
+import base64
 import json
+import re
 import subprocess
 import sys
-import re
-import base64
+from pathlib import Path
 
 RULES_DIR = Path(__file__).parent / "rules"
-DISABLE_KEYS = {
-    'flake8': '--ignore',
-    'ruff': '--ignore',
-    'pylint': '--disable'
-}
+DISABLE_KEYS = {"flake8": "--ignore", "ruff": "--ignore", "pylint": "--disable"}
 LINTER_REGEX = {
-    'flake8': r"^\S+:(\d+):(\d+):\s+([A-Z]\d{3})\s+(.*)$",
-    'ruff': r"^\S+:(\d+):(\d+):\s+([A-Z]+\d+)\s+(.+)$",
-    'pylint': r"^\S+:(\d+):(\d+):\s+([A-Z]\d+)\s+(.+)$"
+    "flake8": r"^\S+:(\d+):(\d+):\s+([A-Z]\d{3})\s+(.*)$",
+    "ruff": r"^\S+:(\d+):(\d+):\s+([A-Z]+\d+)\s+(.+)$",
+    "pylint": r"^\S+:(\d+):(\d+):\s+([A-Z]\d+)\s+(.+)$",
 }
 IS_WINDOWS = sys.platform == "nt"
 PYTHON = sys.executable if IS_WINDOWS else "python"
@@ -35,51 +31,54 @@ PYTHON = sys.executable if IS_WINDOWS else "python"
 
 def load_settings(linter: str) -> dict:
     """Load linter settings from the corresponding JSON file."""
-    with open(RULES_DIR / f"{linter.lower()}.json", 'r') as f:
+    with open(RULES_DIR / f"{linter.lower()}.json", "r") as f:
         return json.load(f)
 
 
 SETTINGS = {
-    'flake8': load_settings('flake8'),
-    'ruff': load_settings('ruff'),
-    'pylint': load_settings('pylint')
+    "flake8": load_settings("flake8"),
+    "ruff": load_settings("ruff"),
+    "pylint": load_settings("pylint"),
 }
 
 
-def encode(disabled: list, linter: str = 'flake8') -> str:
+def encode(disabled: list, linter: str = "flake8") -> str:
     disabled = set(disabled)
     settings = SETTINGS[linter]
 
-    binary = ''.join('1' if code in disabled else '0' for code in settings)  # TODO: O(n): optimise
+    binary = "".join(
+        "1" if code in disabled else "0" for code in settings
+    )  # TODO: O(n): optimise
     encoded = base64.urlsafe_b64encode(
-        int(binary, 2).to_bytes((len(binary) + 7) // 8, 'big')).decode()
+        int(binary, 2).to_bytes((len(binary) + 7) // 8, "big")
+    ).decode()
 
-    return {'link': encoded.strip('=')}
+    return {"link": encoded.strip("=")}
 
 
-def decode(code: str, linter: str = 'flake8') -> dict:
+def decode(code: str, linter: str = "flake8") -> dict:
     settings = SETTINGS[linter]
 
     if code == "":
         return settings
 
     total_errors = len(settings)
-    decoded_bytes = base64.urlsafe_b64decode(code + '==='[:len(code) % 4])
-    n = int.from_bytes(decoded_bytes, 'big')
+    decoded_bytes = base64.urlsafe_b64decode(code + "==="[: len(code) % 4])
+    n = int.from_bytes(decoded_bytes, "big")
     binary = bin(n)[2:].zfill(total_errors)
 
     for index, rule in enumerate(settings):
-        settings[rule]['value'] = binary[index] == '0'
+        settings[rule]["value"] = binary[index] == "0"
 
     return settings
 
 
-def wrap(output: str, linter: str = 'flake8') -> dict[str]:
+def wrap(output: str, linter: str = "flake8") -> dict[str]:
     pattern = re.compile(LINTER_REGEX[linter.lower()], re.MULTILINE)
-    return {'errors': [match.groups() for match in pattern.finditer(output)]}
+    return {"errors": [match.groups() for match in pattern.finditer(output)]}
 
 
-def lint(code: str, disable: list = None, linter: str = 'flake8') -> dict[str]:
+def lint(code: str, disable: list = None, linter: str = "flake8") -> dict[str]:
     linter = linter.lower()
     command = [PYTHON, "-m", linter]
 
@@ -89,25 +88,23 @@ def lint(code: str, disable: list = None, linter: str = 'flake8') -> dict[str]:
     command.append("-")
 
     if disable:
-        command.append(DISABLE_KEYS[linter] + '=' + ','.join(disable))
+        command.append(DISABLE_KEYS[linter] + "=" + ",".join(disable))
 
-    process = subprocess.Popen(command,
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT)
+    process = subprocess.Popen(
+        command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
 
     output = process.communicate(code.encode())[0].decode()
     return wrap(output, linter)
 
 
-def fix(code: str) -> dict[str, str]:
-    process = subprocess.Popen(["autopep8",
-                                "--aggressive",
-                                "--aggressive",
-                                "-"],
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT)
+def fix(code: str) -> dict[str, str]: 
+    process = subprocess.Popen(
+        ["autopep8", "--aggressive", "--aggressive", "-"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
     output = process.communicate(code.encode())[0].decode()
 
     return {"code": output}
